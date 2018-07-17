82687b0777e6a0a6056319bbbd4fe05d5c08fbbe
matseda <matseda@stud.ntnu.no>
2018-07-17 13:20:30 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Logging;
+
+// namespace radix_dotnet_v3
+// {
+//     public class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             CreateWebHostBuilder(args).Build().Run();
+//         }
+
+//         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+//             WebHost.CreateDefaultBuilder(args)
+//                 .UseStartup<Startup>();
+//     }
+// }
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace radix_dotnet_v3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
